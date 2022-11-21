59042fe9ce6faf24fde0488c0c4dec342e67be3b
YamanakaRui <k021g1123@g.neec.ac.jp>
2022-11-21 09:31:45 +0900

    完成

---
+++ SceneManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+enum class Scene 
+{
+	Title,
+	NewPlay,
+	GamePlay,
+	GameClear,
+};
+
+class SceneManager final
+{
+private:
+	Scene current_ = Scene::Title;
+	Scene next_ = Scene::NewPlay;
+public:
+	void Change(const Scene& scene);
+	void DrawNext();
+public:
+	static SceneManager* GetInstance();
+private:
+	SceneManager() = default;
+	~SceneManager() = default;
+	SceneManager(const SceneManager& obj) = delete;
+	SceneManager& operator=(const SceneManager& obj) = delete;
+};
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include "SceneManager.h"
 
 int main()
 {
+	SceneManager* sceneMan = SceneManager::GetInstance();
+
+	sceneMan->DrawNext();
+	sceneMan->Change(Scene::NewPlay);
+
+	sceneMan->DrawNext();
+	sceneMan->Change(Scene::GamePlay);
+
+	sceneMan->DrawNext();
+	sceneMan->Change(Scene::GameClear);
+
+	sceneMan->DrawNext();
 
 	return 0;
 }
